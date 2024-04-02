051a599d4e1591c95b023a8cb81ead7344586825
shridhar <61188528+shridhar-benni@users.noreply.github.com>
2024-04-02 22:17:13 +0530

    Update ISF.py

--- scripts/ISF.py
+++ scripts/ISF.py
@@ -9,25 +9,58 @@ import argparse
 import os
 import platform as pt
 import json as json
+from datetime import datetime
+import uuid
+
+
+def generate_unique_id():
+    # Generate a unique identifier
+    unique_id = uuid.uuid4().hex[:8]  # Using the first 8 characters of a UUID
+
+    # Construct the image name
+    image_name = f"{unique_id}.jpg"
+
+    return image_name
+
+
+#functio to get time stamp
+def get_time_stamp():
+    # Get the current timestamp
+    timestamp = datetime.timestamp(datetime.now())
+
+    # Convert the timestamp to a datetime object
+    dt_object = datetime.fromtimestamp(timestamp)
+
+    # Format the datetime object as date-month-year time
+    formatted_time = dt_object.strftime("%d-%m-%Y %H:%M:%S")
+
+    return formatted_time
 
 #function to write result as json file
-def write_to_json(result, image, oj):
+def write_data(result, image_id, oj):
     #print(result.boxes.numpy())
+    boxes = result.boxes.xywh.tolist()
+    b_areas = []
+    
+    for box in boxes:
+        b_areas.append(box[3]*box[2])
+        
+    
     dictionary = {
+        "timestamp": get_time_stamp(),
+        "image_id": image_id,
         "cls": result.boxes.cls.tolist(),
         "conf": result.boxes.conf.tolist(),
-        "boxes": result.boxes.xywh.tolist(),
+        "areas": b_areas,
     }
+    
+    with open(oj + 'data.txt', 'a') as data_file: 
+        data_file.write(json.dumps(dictionary)+'\n')
  
-    # Serializing json
-    json_object = json.dumps(dictionary, indent=4)
- 
-    # Writing to sample.json
-    with open(oj + image[:-3] + "json", "w") as outfile:
-        outfile.write(json_object)
+    
 
 #function to run inference
-def run_inference_on_images(images_path, model_path, output_path, output_json):
+def run_inference_on_images(images_path, model_path, output_img_path, output_json_path):
     model = YOLO(model_path, task='segment')
 
     # Process each image in the list
@@ -36,53 +69,40 @@ def run_inference_on_images(images_path, model_path, output_path, output_json):
         results = model.predict(images_path+image, conf=0.5)
 
         # Display and save the results
-        for r in results:
-            write_to_json(r, image, output_json)
-            im_array = r.plot()
+        for result in results:
+            image_id = generate_unique_id()
+            write_data(result, image_id, output_json_path)
+            im_array = result.plot()
             im = Image.fromarray(im_array[..., ::-1])
             #display(im)
-            im.save(output_path+image) #to save image
+            im.save(output_img_path+image_id) #to save image
 
 
 if __name__ == '__main__':
-    
-    default_in = ""
-    default_model = ""
-    default_out = ""
-    if pt.system() == 'Windows':
-        default_in = "..\input_images\\"
-        default_model = "..\models\\yolov8n-seg.pt"
-        default_out = "..\output_images\\"
-        default_json = "..\output_json\\"
-    else:
-        default_in = "../input_images/"
-        default_model = "../models/yolov8n-seg.pt"
-        default_out = "../output_images/"
-        default_json = "../output_json/"
         
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '-i',
         '--in_imgs_path',
-        default=default_in,
+        default="../input_images/",
         help='folder path containing image/images for detection.')
         
     parser.add_argument(
         '-m',
         '--model_file',
-        default=default_model,
+        default="../models/V3/best_full_integer_quant.tflite",
         help='model to be executed')
     
     parser.add_argument(
         '-o',
         '--out_imgs_path',
-        default=default_out,
+        default="../output_images/",
         help='path to store detected images')
     
     parser.add_argument(
         '-oj',
         '--out_json_path',
-        default=default_json,
+        default="../output_text/",
         help='path to store result of detected images')
 
     args = parser.parse_args()
